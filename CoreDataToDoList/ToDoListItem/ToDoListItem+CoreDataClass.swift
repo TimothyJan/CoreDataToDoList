90720ea55cddfee0272e0791322945a7f0bf3bff
TimothyJan <jan.timothy@gmail.com>
2021-11-27 20:48:36 -0800

    ToDoList complete

---
+++ CoreDataToDoList/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  CoreDataToDoList
+//
+//  Created by Timothy Jan on 11/27/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
